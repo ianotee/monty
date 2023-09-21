3eb92cc6d53cc01525a2b032201b36f285fb7881
Planter44 <planterplanter777@gmail.com>
2023-09-21 12:41:29 -0700

    28

--- main.c
+++ main.c
@@ -45,3 +45,70 @@ int main(int argc, char *argv[])
 	fclose(stream);
 	exit(EXIT_SUCCESS);
 }
+
+/**
+ * get_opcode - reads opcode and verifies if is valid.
+ * @stack: double pointer to header (top) of the stack.
+ * @line_number: counter for line number of the file.
+ * @code: opcode to excecute.
+ *
+ * Return: void.
+ */
+void get_opcode(stack_t **stack, unsigned int line_number, char *code)
+{
+	int i = 0;
+	instruction_t opcode_func[] = {
+		{"add", _add},
+		{"div", _div},
+		{"mod", _mod},
+		{"mul", _mul},
+		{"nop", _nop},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"push", _push},
+		{"sub", _sub},
+		{"swap", _swap},
+		{"pchar", _pchar},
+		{"pstr", _pstr},
+		{"rotr", _rotr},
+		{"rotl", _rotl},
+		{"stack", _stack},
+		{"queue", _queue},
+		{NULL, NULL}
+	};
+
+	while (opcode_func[i].opcode)
+	{
+		if (strcmp(opcode_func[i].opcode, code) == 0)
+		{
+			opcode_func[i].f(stack, line_number);
+			return;
+		}
+		i++;
+	}
+	dprintf(STDERR_FILENO, "L%u: unknown instruction %s\n", line_number, code);
+	free_stack_t(*stack);
+	/**
+	 * close_file
+	 */
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * free_stack_t - function that free a list of type dlistint_t
+ * @head: pointer to a list type stack_t
+ *
+ * Return: void.
+ */
+void free_stack_t(stack_t *head)
+{
+	stack_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
